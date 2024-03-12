10b0898b1cd1dc5693f8634b81e59b79a6c2177b
CoderJourney-code <138033988+CoderJourney-code@users.noreply.github.com>
2024-03-11 17:58:37 -0700

    Have image in spotify player
    Have image in spotify player along with far better gradient

--- docs/app.js
+++ docs/app.js
@@ -9,6 +9,17 @@ TODO: Have fresh token and logout be seperated some more.
 
 */
 
+// window.Spotify.getPlayerInstances().forEach(player => {
+//     player.disconnect();
+// });
+
+// window.addEventListener('beforeunload', function() {
+//     window.Spotify.getPlayerInstances().forEach(player => {
+//         player.disconnect();
+//     });
+// });
+
+
 export function playerFunction(accessToken) {
     console.log("Web player is ready!")
     let token = accessToken;
@@ -26,7 +37,12 @@ export function playerFunction(accessToken) {
     // Ready
     player.addListener('ready', ({ device_id }) => {
         console.log('Ready with Device ID', device_id);
+        player.getCurrentState().then(state => {
+            if (!state) { return; } 
+            
+        });
         startCallbackInterval();
+        
     });
 
     // Not Ready
@@ -92,14 +108,42 @@ export function playerFunction(accessToken) {
             if(currentPosition < start_sec || currentPosition > end_sec) {
                 player.seek(start_sec*1000);
             }
+
         });
     }
+
+    player.addListener('player_state_changed', state => {
+        document.getElementById('current-track').src = state.track_window.current_track.album.images[0].url;
+        document.getElementById('current-track-name').textContent = state.track_window.current_track.name;
+    });
     
     function startCallbackInterval() { 
         intervalId = setInterval(() => checkAndSeek(player), 500);
         console.log(intervalId)
     }
+    // function checkAndSeek(state) {
+    //     if (!state) {
+    //         console.error('User is not playing music through the Web Playback SDK');
+    //         return;
+    //     }
+    //     let [start_min, start_sec] = document.getElementById('start_time').value.split(':');
+    //     let [end_min, end_sec] = document.getElementById('end_time').value.split(":");
+    //     start_min = parseFloat(start_min);
+    //     start_sec = parseFloat(start_sec);
+    //     end_min = parseFloat(end_min);
+    //     end_sec = parseFloat(end_sec);
+    //     start_sec += start_min*60;
+    //     end_sec += end_min*60;
+    //     let currentPosition = state.position / 1000;
+    //     if(currentPosition < start_sec || currentPosition > end_sec) {
+    //         player.seek(start_sec*1000);
+    //     }
+    //     console.log(end_sec);
+
+    // }
     
+    // player.addListener('player_state_changed', checkAndSeek);
+
     player.connect();
 
     };
